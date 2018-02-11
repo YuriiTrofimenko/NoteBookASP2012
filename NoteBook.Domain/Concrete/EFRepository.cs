0f72a2b370acf9c332fa7fa4f04e8c34edeecf3a
student <student>
2018-02-11 14:58:38 +0200

    init

---
+++ NoteBook.Domain/Concrete/EFRepository.cs
@@ -0,0 +1,20 @@
+﻿using NoteBook.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoteBook.Domain.Entity;
+
+namespace NoteBook.Domain.Concrete
+{
+    public class EFRepository : IRepository
+    {
+        private EFDbContext db = new EFDbContext();
+
+        //private NoteBookEntities db = new NoteBookEntities();
+
+        public IQueryable<State> States { get { return db.States;} }
+        public IQueryable<AnOrder> Orders { get { return db.AnOrders; } }
+    }
+}
